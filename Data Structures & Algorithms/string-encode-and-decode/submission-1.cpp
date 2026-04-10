e5bbb36c8a0de07a496367104fc2cbff02895db2
Radiy Aseev <137694775+RLM2588@users.noreply.github.com>
2026-04-10 13:29:59 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        size_t total = 1;
+        for(const auto& it: strs) {
+            total += 1 + it.length();
+        }
+
+        string answ;
+        answ.reserve(total);
+
+        uint8_t count = static_cast<uint8_t>(strs.size());
+        answ.push_back(static_cast<char>(count));
+
+        for(string& it: strs) {
+            uint8_t len = static_cast<uint8_t>(it.length());
+            answ.push_back(static_cast<char>(len));
+            answ.append(it.data(), it.length());
+        }
+        return answ;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> answ;
+        const char* ptr = s.data();
+        const char* end = ptr + s.size();
+        uint8_t count = static_cast<uint8_t>(*ptr);
+        ++ptr;
+
+        for(int i = 0; i < count; ++i) {
+            uint8_t len = static_cast<uint8_t>(*ptr);
+            ++ptr;
+            if (ptr + len > end) break;
+            answ.emplace_back(ptr, len);
+            ptr += len;
+        }
+        return answ;
+    }
+};
