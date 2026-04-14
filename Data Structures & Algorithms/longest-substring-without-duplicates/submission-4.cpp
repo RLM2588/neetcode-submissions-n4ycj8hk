663825544d04d96fd27abdf56c8335cac7219c97
Radiy Aseev <137694775+RLM2588@users.noreply.github.com>
2026-04-14 18:57:09 +0300

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 1;
+        if(s.size() < 2) return s.size();
+        auto l = 0;
+        auto r = 1;
+        unordered_map<char, int> hash;
+        hash[s[l]] = 1;
+        while(r < s.size()) {
+            if (hash[s[r]] < l + 1) {
+                hash[s[r]] = r + 1;
+                maxlen = max(maxlen, r - l + 1);
+                ++r;
+            } else {
+                if(r + 1 >= s.size()) return maxlen;
+                l = hash[s[r]];
+                hash[s[r]] = r + 1;
+                maxlen = max(maxlen, r - l + 1);
+                ++r;
+            }
+        }
+        return maxlen;
+    }
+};
